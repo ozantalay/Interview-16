80f3cee4ab835b387e9f9fce50221a6563605d46
ozantalay <ozntalay@hotmail.com>
2024-10-07 00:48:50 +0300

    baslangic

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+
+function App() {
+  const PLAYERS = [
+    "Ali",
+    "Namık",
+    "Eda",
+    "Ebru",
+    "Suzan",
+    "Samet",
+    "Engin",
+    "Halit"
+  ];
+
+  return <FormTeams players={PLAYERS} />;
+}
+
+const FormTeams = ({ players }) => {
+  // KODUNUZ BURAYA GELECEK
+};
+
+export default App;
